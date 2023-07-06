102c25024fd6df737ce421b0e548a356a90a649e
Thomas Ströder <thomas.stroeder@email.de>
2023-07-06 12:45:40 +0100

    skeleton for immutable set

---
+++ src/smalecture/ImmutableSet.java
@@ -0,0 +1,67 @@
+package smalecture;
+
+import java.util.*;
+
+public class ImmutableSet<T> {
+
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public <E> E[] toArray(E[] a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ImmutableSet<T> add(T e) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ImmutableSet<T> remove(T e) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public ImmutableSet<T> addAll(Collection<? extends T> c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ImmutableSet<T> retainAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ImmutableSet<T> removeAll(Collection<? extends T> c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
